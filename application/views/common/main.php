ffceb6bfd70a7aaad731662d9904ed16a110814c
Israfilova Elena <malvinkooo@gmail.com>
2019-02-22 17:30:46 +0200

    Minor changes in views

--- application/views/common/main.php
+++ application/views/common/main.php
@@ -19,22 +19,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<!-- Site Title -->
 		<title>Magazine</title>
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
-		<!--
-		CSS
-		============================================= -->
-		<link rel="stylesheet" href="/assets/css/linearicons.css">
-		<link rel="stylesheet" href="/assets/css/font-awesome.min.css">
-		<link rel="stylesheet" href="/assets/css/bootstrap.css">
-		<link rel="stylesheet" href="/assets/css/magnific-popup.css">
-		<link rel="stylesheet" href="/assets/css/nice-select.css">
-		<link rel="stylesheet" href="/assets/css/animate.min.css">
-		<link rel="stylesheet" href="/assets/css/owl.carousel.css">
-		<link rel="stylesheet" href="/assets/css/jquery-ui.css">
-		<link rel="stylesheet" href="/assets/css/main.css">
+
+		<?=include 'styles.php'?>
+
 	</head>
 	<body>
 		<header>
-			
+
 			<div class="header-top">
 				<div class="container">
 					<div class="row">
@@ -103,7 +94,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</div>
 			</div>
 		</header>
-		
+
 		<div class="site-main-container">
 			<!-- Start top-post Area -->
 			<section class="top-post-area pt-10">
@@ -287,7 +278,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								</div>
 							</div>
 							<!-- End latest-post Area -->
-							
+
 							<!-- Start banner-ads Area -->
 							<div class="col-lg-12 ad-widget-wrap mt-30 mb-30">
 								<img class="img-fluid" src="/assets/img/banner-ad.jpg" alt="">
@@ -648,89 +639,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</section>
 			<!-- End latest-post Area -->
 		</div>
-		
-		<!-- start footer Area -->
-		<footer class="footer-area section-gap">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-3 col-md-6 single-footer-widget">
-						<h4>Top Products</h4>
-						<ul>
-							<li><a href="#">Managed Website</a></li>
-							<li><a href="#">Manage Reputation</a></li>
-							<li><a href="#">Power Tools</a></li>
-							<li><a href="#">Marketing Service</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Quick Links</h4>
-						<ul>
-							<li><a href="#">Jobs</a></li>
-							<li><a href="#">Brand Assets</a></li>
-							<li><a href="#">Investor Relations</a></li>
-							<li><a href="#">Terms of Service</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Features</h4>
-						<ul>
-							<li><a href="#">Jobs</a></li>
-							<li><a href="#">Brand Assets</a></li>
-							<li><a href="#">Investor Relations</a></li>
-							<li><a href="#">Terms of Service</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-2 col-md-6 single-footer-widget">
-						<h4>Resources</h4>
-						<ul>
-							<li><a href="#">Guides</a></li>
-							<li><a href="#">Research</a></li>
-							<li><a href="#">Experts</a></li>
-							<li><a href="#">Agencies</a></li>
-						</ul>
-					</div>
-					<div class="col-lg-3 col-md-6 single-footer-widget">
-						<h4>Instragram Feed</h4>
-						<ul class="instafeed d-flex flex-wrap">
-							<li><img src="/assets/img/i1.jpg" alt=""></li>
-							<li><img src="/assets/img/i2.jpg" alt=""></li>
-							<li><img src="/assets/img/i3.jpg" alt=""></li>
-							<li><img src="/assets/img/i4.jpg" alt=""></li>
-							<li><img src="/assets/img/i5.jpg" alt=""></li>
-							<li><img src="/assets/img/i6.jpg" alt=""></li>
-							<li><img src="/assets/img/i7.jpg" alt=""></li>
-							<li><img src="/assets/img/i8.jpg" alt=""></li>
-						</ul>
-					</div>
-				</div>
-				<div class="footer-bottom row align-items-center">
-					<p class="footer-text m-0 col-lg-8 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-					<div class="col-lg-4 col-md-12 footer-social">
-						<a href="#"><i class="fa fa-facebook"></i></a>
-						<a href="#"><i class="fa fa-twitter"></i></a>
-						<a href="#"><i class="fa fa-dribbble"></i></a>
-						<a href="#"><i class="fa fa-behance"></i></a>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!-- End footer Area -->
-		<script src="/assets/js/vendor/jquery-2.2.4.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="/assets/js/vendor/bootstrap.min.js"></script>
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-		<script src="/assets/js/easing.min.js"></script>
-		<script src="/assets/js/hoverIntent.js"></script>
-		<script src="/assets/js/superfish.min.js"></script>
-		<script src="/assets/js/jquery.ajaxchimp.min.js"></script>
-		<script src="/assets/js/jquery.magnific-popup.min.js"></script>
-		<script src="/assets/js/mn-accordion.js"></script>
-		<script src="/assets/js/jquery-ui.js"></script>
-		<script src="/assets/js/jquery.nice-select.min.js"></script>
-		<script src="/assets/js/owl.carousel.min.js"></script>
-		<script src="/assets/js/mail-script.js"></script>
-		<script src="/assets/js/main.js"></script>
-	</body>
-</html>
+
+<?= include 'footer.php'; ?>
